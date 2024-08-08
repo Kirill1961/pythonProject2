c6022647d842943f229b02d622521ea8de4f0aed
Kirill <julich36ru@gmail.com>
2024-08-08 23:03:23 +0300

    09 08 24 REGEX pattern for data YYYY/MM/DD

--- re_phone_format.py
+++ re_phone_format.py
@@ -147,6 +147,7 @@ def find_dates_in_text_3(text):
     # mnt = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     mnt = {'January': '01', 'February': '02', 'March': '03', 'April': '04', 'May': '05', 'June': '06', 'July': '07',
            'August': '08', 'September': '09', 'October': '10', 'November': '11', 'December': '12'}
+
     day_week = {'Mon': '01',
                 'Tue': '02',
                 'Wed': '03',

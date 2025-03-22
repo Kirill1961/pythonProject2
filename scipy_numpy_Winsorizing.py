8781076494420cb593d737b2a0fcc8302ab2ff2c
Kirill <julich36ru@gmail.com>
2025-03-22 19:35:12 +0300

    22 03 25 chi2 Cramers V

--- scipy_numpy_Winsorizing.py
+++ scipy_numpy_Winsorizing.py
@@ -20,7 +20,7 @@ print(f'Среднее до усечения :{sales_before:.2F}')
 fig, axes = plt.subplots(figsize=(2, 2))
 sns.boxplot(x=df.Sales)
 axes.set_title(f'До усечения выборки :{sales_before:.2F}')
-plt.show()
+# plt.show()
 
 sales_after = stats.trim_mean(df.Sales, 0.25)
 print(f'Среднее после усечения :{sales_after:.2F}')
@@ -29,7 +29,7 @@ sample_after = stats.trimboth(df.Sales, 0.3)
 fig, axes = plt.subplots(figsize=(2, 2))
 axes.set_title(f'После усечения выборки :{sales_after:.2F}')
 sns.boxplot(x=sample_after)
-plt.show()
+# plt.show()
 
 # TODO Фильтр bool столбца Sales по МОДАЛЬНОМУ значению = 12.96
 mode = df.Sales.mode()[0]  # mode - мах повтор значения
@@ -49,3 +49,10 @@ df.Sales.mode()
 print(len(df["Sales"]))
 df["Sales"] = winsorize(df["Sales"], limits=[0.05, 0.05])
 print(len(df["Sales"]))
+
+# TODO Мода в NumPy -> только для целых чисел, используем np.bincount() + np.argmax()
+arr = np.array([7, 7, 7, 7, 7, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4])
+
+mode_np = np.bincount(arr).argmax()
+print("Мода (NumPy):", mode_np)  # 4
+

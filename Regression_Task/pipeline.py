796be5f9700d71082f3790d08a93e34907ec5d30
Kirill <julich36ru@gmail.com>
2025-05-28 00:41:54 +0300

    27 05 25 Генерация данных с весами

--- Regression_Task/pipeline.py
+++ Regression_Task/pipeline.py
@@ -8,7 +8,9 @@ from sklearn.model_selection import train_test_split
 X = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]])
 y = np.array([1, 2, 3, 4, 5])
 
-# Разделение данных на тренировочный и тестовый наборы
+# TODO Разделение данных на тренировочный и тестовый наборы
+#  * test_size=0.2	20% — в тест, 80% — в обучение
+#  * random_state	фиксирует случайное разделение
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Определение пайплайна

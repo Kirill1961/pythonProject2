80031c72683f08fcce01c81d719e336e4952a895
Kirill1961 <julich36ru@gmail.com>
2025-08-09 14:03:53 +0300

    09 08 25 pytorch office

--- pytorch_1.py
+++ pytorch_1.py
@@ -4,25 +4,29 @@ import torch.optim as optim
 
 # Входные и целевые данные
 x = torch.tensor([[0.5, 0.8]], requires_grad=True)  # входной слой
-y_target = torch.tensor([[1.0]])  # целевое значение
+y_target = torch.tensor([[1., 0.]])  # целевое значение
 
 # Модель: 2 входа → скрытый слой из 3 нейронов → 1 выход
 model = nn.Sequential(
-    nn.Linear(2, 3),  # w_h
+    nn.Linear(2, 3, bias=True),  # w_h
     nn.ReLU(),
-    nn.Linear(3, 1)   # w_y
+    nn.Linear(3, 3, bias=True),
+    nn.ReLU(),
+    nn.Linear(3, 2),   # w_y
 )
+
 
 # Функция потерь и оптимизатор
 loss_fn = nn.MSELoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01)
 
 # Шаг обучения (одно обновление)
-y_pred = model(x)                         # прямое распространение
-loss = loss_fn(y_pred, y_target)         # вычисление ошибки
-loss.backward()                          # обратное распространение (autograd)
-optimizer.step()                         # обновление весов
-optimizer.zero_grad()                    # обнуляем градиенты
+for epoch in range(200):
+    y_pred = model(x)                         # прямое распространение
+    loss = loss_fn(y_pred, y_target)         # вычисление ошибки
+    loss.backward()                          # обратное распространение (autograd)
+    optimizer.step()                         # обновление весов
+    optimizer.zero_grad()                    # обнуляем градиенты
 
 print(y_pred)
 

85fa518d520e02c87740bf4e45fef4d162dfa4ad
Kirill <julich36ru@gmail.com>
2026-03-13 23:53:58 +0300

    13 03 26 taxi train test

--- taxi_etna.py
+++ taxi_etna.py
@@ -21,15 +21,24 @@ from statsmodels.tsa.seasonal import seasonal_decompose, STL
 date_rng = pd.date_range("2018-03-01", "2018-08-31 23:50", freq="10min")
 
 np.random.seed(42)
-orders = np.random.poisson(lam=30, size=len(date_rng))
+# orders = np.random.poisson(lam=30, size=len(date_rng))
+orders = np.sort(np.random.poisson(lam=30, size=len(date_rng)))
 
 trend = 0.0001 * np.arange(len(orders))
 
-# season_day = 8 * np.sin(2 * np.pi * np.arange(len(orders)) / 72)
+# trend = 0
 
-season_day = 0
+# orders = 0.2 * orders + (1 - 0.2) * orders
+
+
+# season_day = 5 * np.sin(2 * np.pi * np.arange(len(orders)) / 144)
+season_day = 5 * np.sin(2 * np.pi * np.arange(len(orders)) / 144)
+
+
+# season_day = 0
 
 orders_trend = orders + trend + season_day
+
 
 df = pd.DataFrame({"num_orders": orders_trend}, index=date_rng)
 
@@ -49,7 +58,7 @@ print(df.min(), df.max())
 #%%
 # TODO Декомпозиция через STL
 
-stl = STL(endog=df, period=100)
+stl = STL(endog=df["num_orders"], period=144)
 
 res = stl.fit()
 
@@ -57,9 +66,22 @@ res.plot()
 plt.show()
 
 #%%
-# TODO Декомпозиция через STL
+# TODO Декомпозиция через acf
 
 plot_acf(df.num_orders, lags=30)
 plt.show()
 
 #%%
+# TODO frequency & period
+print(pd.infer_freq(df.index))
+df.diff().value_counts()
+
+#%%
+# TODO train и test через percentile
+train_size = np.percentile(df, 90)
+train = df[df['num_orders'] < train_size]
+test = df[~(df['num_orders'] < train_size)]
+
+#%%
+# TODO target
+

37f636873915a8df50f535d7114a2d3876e54312
Kirill1961 <julich36ru@gmail.com>
2026-05-26 15:15:44 +0300

    26 05 26 офис

--- ab_merge.py
+++ ab_merge.py
@@ -5,12 +5,19 @@ A/B test
 то есть количество пользователей, решивших оплатить продукт компании.
 Ваша задача — с помощью этого блокнота помочь компании понять, следует ли ей внедрить новую страницу,
 оставить старую или, возможно, продлить эксперимент, чтобы принять окончательное решение.
+ * id - уникальный идентификатор пользователя
+ * time - время события/визита пользователя
+ * con_treat - (или чаще group)	в какую группу эксперимента попал пользователь: control или treatment
+ * page - какую версию страницы увидел пользователь
+ * converted - совершил ли пользователь целевое действие
+ * country - страна пользователя
 """
 #%%
 import numpy as np
 import pandas as pd
 from scipy.stats import norm
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 #%%
 # TODO data
@@ -23,7 +30,32 @@ print(ab_test.shape, '\n', countries.shape)
 data = pd.merge(ab_test, countries, on='id', how='left')
 data.shape
 
+
 # TODO Число дубликатов
 #%%
 duplicate = data.duplicated(subset=['id']).sum()
 duplicate
+
+
+#%%
+# data[data['country'] == 'US']['id'].unique().shape
+# data[data['country'] == 'US']['id']
+cnt = data.groupby('country')['id'].count()
+print(cnt)
+
+# plt.hist(cnt.values)
+# plt.show()
+
+sns.histplot(data['country'])
+plt.show()
+
+
+
+#%%
+plt.hist(data['country'])
+# for n, name in enumerate(data['country'].unique()):
+#    print( data[data['country'] == name]['id'].count())
+#    # plt.hist(data[data['country'] == name]['id'].count(), bins=30)
+plt.show()
+
+
